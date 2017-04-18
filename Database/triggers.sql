81052fe15f818a8232e9d5f212f50273642f7aab
Mathieu SOYER <mathieu.soyer@etu.univ-nantes.fr>
2017-04-18 14:44:21 +0200

    ajout fichier de config + trigger: evenement a lieu dans une ecole ou a une adresse donnee

--- Database/triggers.sql
+++ Database/triggers.sql
@@ -14,3 +14,21 @@ BEGIN
 END; //
 
 DELIMITER ;
+
+-- #######################################################################
+-- l'événement à lieu dans une école ou à une adresse donnée
+-- #######################################################################
+CREATE TRIGGER event_at_a_school_or_at_a_given_address
+BEFORE INSERT
+   ON EVENT FOR EACH ROW
+BEGIN
+    IF new.address IS NULL AND new.school_id IS NULL THEN -- si il n'y a pas d'adresse NI d'école désignée
+        SET msg = concat('event_at_a_school_or_at_a_given_addressERROR: address and school_id are null: ', cast(new.id AS CHAR));
+        signal sqlstate '45001' SET message_text = msg;
+    ELSE IF new.address IS NOT NULL AND new.school_id IS NOT NULL THEN -- si il y a une adresse ET une école désignée
+        SET msg = concat('event_at_a_school_or_at_a_given_addressERROR: address and school_id are not null: ', cast(new.id AS CHAR));
+        signal sqlstate '45002' SET message_text = msg;
+    END IF;
+END; //
+
+DELIMITER ;
