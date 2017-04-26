91b060dc6f5812d0c764706f5dd1a77a6fa47699
PierreSAVATTE <E148834B@etu.univ-nantes.fr>
2017-04-26 18:15:20 +0200

    Creation des boutons pour l'application (modification du look-and-feel

--- app/EventaryButton.java
+++ app/EventaryButton.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
-import java.awt.Graphics2D;
+import java.awt.*;
 
 public class EventaryButton extends JButton{
 
   EventaryButton(String str){
     super(str);
+    this.setBackground(new Color(225,225,225));
+    this.setForeground(Color.BLACK);
+    this.setFocusPainted(false);
+    this.setFont(new Font("Arial", Font.BOLD, 12));
+    //this.setBorderPainted(false);
+      //Enleve les bordures des boutons
+    //this.setRolloverEnabled(false);
+      //Enleve l'effet hoover
   }
 }
--- app/Screen.java
+++ app/Screen.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.BorderLayout;
+import java.awt.*;
 
 public class Screen extends JFrame{
 
@@ -14,14 +14,18 @@ public class Screen extends JFrame{
     this.setTitle("Eventary");
     //Définit sa taille : 400 pixels de large et 100 pixels de haut
     this.setSize(1440,810);
+
+    this.setBackground(new Color(240,240,240));
     //Nous demandons maintenant à notre objet de se positionner au centre
     this.setLocationRelativeTo(null);
     //Termine le processus lorsqu'on clique sur la croix rouge
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JButton bouton = new JButton("Hello World");
+    JButton bouton = new EventaryButton("Hello World");
+    JButton bouton2 = new EventaryButton("Hello World");
     bouton.addMouseListener(new ListenerPrintHello());
     panel.add(bouton, BorderLayout.CENTER);
+    panel.add(bouton2, BorderLayout.CENTER);
     this.setContentPane(panel);
 
 
