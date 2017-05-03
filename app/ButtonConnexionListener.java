7a08f98a6feca41a81104c2722bf657042d5187d
PierreSAVATTE <E148834B@etu.univ-nantes.fr>
2017-05-03 18:03:30 +0200

    Fenetre de connexion et debut d'ajout d'une seconde page

---
+++ app/ButtonConnexionListener.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class ButtonConnexionListener implements ActionListener {
+	
+	private Screen screen;
+	
+	ButtonConnexionListener(Screen screen){
+		this.screen = screen;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e){
+		System.out.println("Connexion");
+		this.screen.getConnected();
+	}
+	
+}
