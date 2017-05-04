cb226bc3f6eac0bb6e5606c21845b7dd8e67d5b0
Mathieu SOYER <mathieu.soyer@etu.univ-nantes.fr>
2017-05-04 21:17:55 +0200

    Test SiteDAO et SchoolDAO

--- src/Test/DaoTest.java
+++ src/Test/DaoTest.java
@@ -1,12 +1,19 @@
 package Test;
 
 import java.util.ArrayList;
-
 import database.*;
 import model.*;
 
+/**
+ * The Class DaoTest.
+ */
 public class DaoTest {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("\n\t########## test de RoomDAO");
 		RoomDAO dao1 = new RoomDAO();
@@ -32,5 +39,31 @@ public class DaoTest {
 		System.out.println("\n\t##### test de find");
 		Building building2 = dao2.find(2);
 		System.out.println(building2.toString());
+		
+		
+		System.out.println("\n\t########## test de SiteDAO");
+		SiteDAO dao3 = new SiteDAO();
+		System.out.println("\n\t##### test de findAll");
+		ArrayList<Site> sites = new ArrayList<Site>();
+		sites = dao3.findAll();
+		for(Site s: sites){
+			System.out.println(s.toString());
+		}
+		System.out.println("\n\t##### test de find");
+		Site site3 = dao3.find(3);
+		System.out.println(site3.toString());
+		
+		
+		System.out.println("\n\t########## test de SchoolDAO");
+		SchoolDAO dao4 = new SchoolDAO();
+		System.out.println("\n\t##### test de findAll");
+		ArrayList<School> schools = new ArrayList<School>();
+		schools = dao4.findAll();
+		for(School s: schools){
+			System.out.println(s.toString());
+		}
+		System.out.println("\n\t##### test de find");
+		School school1 = dao4.find(1);
+		System.out.println(school1.toString());
 	}
 }
