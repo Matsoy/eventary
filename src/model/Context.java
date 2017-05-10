5649b68fe93d4694dedd7c61604ca0003ed5a3ad
Neil Rittner <neil.rittner@etu.univ-nantes.fr>
2017-05-10 19:15:43 +0200

    Modification méthode de lecture du tempsAvantSuppression dans config

--- src/model/Context.java
+++ src/model/Context.java
@@ -1,19 +1,19 @@
 package model;
 
 import database.*;
-import java.io.File;
+import java.io.*;
 
 public class Context {
 	User currentUser;
 	boolean authentification;	//Sert à savoir si l'authentification à réussi
 	int tempsAvantSuppression;
-	
+
 	public Context(){
 		this.currentUser = null;
 		this.authentification = false;
 		this.tempsAvantSuppression = lireTempsAvantSupp();
 	}
-	
+
 	public int getTempsAvantSuppression() {
 		return tempsAvantSuppression;
 	}
@@ -24,12 +24,34 @@ public class Context {
 
 	public int lireTempsAvantSupp(){
 		int temps = 0;
-		File config = new File("../../config.txt");
-		System.out.println("Chemin absolu du fichier : " + config.getAbsolutePath());
-		
+		String currentLine = "";	// ligne courante
+		String S = "DurationBeforeDeletion";	// Chaine à retrouver pour avoir le temps
+		int ctr = 0;	// Variable de controle
+
+		try {     
+			BufferedReader br = new BufferedReader(new FileReader("config.txt"));	// Ouverture du fichier pour lecture
+			//lecture du fichier texte 
+			while (((currentLine = br.readLine()) != null) || ctr == 0){
+				if (currentLine.startsWith(S)){   // on a la ligne qui indique le tempsAvantSuppression
+					int index = currentLine.indexOf(":");
+					temps = Integer.parseInt(currentLine.substring(index +2));
+					System.out.println(temps);
+					ctr++;
+				}  
+			}
+
+			if(ctr == 0){
+				System.out.println("ERROR : pas de ligne DurationBeforeDeletion");
+			}
+			br.close();	// On ferme le flux
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.toString());
+		}
 		return temps;
 	}
-	
+
 	public boolean connexion(String login, String passwd){
 		UserDAO user = new UserDAO();
 		this.currentUser = user.connect(login, passwd);
@@ -40,9 +62,8 @@ public class Context {
 		this.authentification = true;
 		return this.authentification;
 	}
-	
-	public static void main(String[] args) {	// Servira à tester la lecture dans le fichier de config
-		
-	}
-	
+
+//	public static void main(String[] args) {	// Servira à tester la lecture dans le fichier de config
+//		Context context = new Context();
+//	}
 }
