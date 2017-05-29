08b1cea405b6a12606148014c9995c4a1cc1561a
Neil Rittner <neil.rittner@etu.univ-nantes.fr>
2017-05-29 13:22:49 +0200

    Raffraichissement des orgas

--- src/controller/ActionOrganisations.java
+++ src/controller/ActionOrganisations.java
@@ -6,6 +6,7 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.SwingUtilities;
 
+import database.OrganizationDAO;
 import model.Context;
 import view.HomePanel;
 
@@ -41,6 +42,7 @@ public class ActionOrganisations extends AbstractAction{
 	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		model.getCurrentUser().setListeAsso(OrganizationDAO.assoInWhichUserIsMember(model.getCurrentUser().getLogin()));
 		this.view.setContainerCentral(this.view.getFrame().getMyOrgaPanel());
 		this.view.getFrame().getMyOrgaPanel().displayMyOrganisations(model.getCurrentUser().getListeAsso(), model.getCurrentUser().getListeDpt());
 		SwingUtilities.updateComponentTreeUI(this.view.getFrame());
